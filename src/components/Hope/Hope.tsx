cfef9cc6c693abf3613ff64b901d84c3a582f66e
vinhdt912 <dothanhvinh0912@gmail.com>
2023-10-25 23:05:19 +0700

    add

--- src/components/Hope/Hope.tsx
+++ src/components/Hope/Hope.tsx
@@ -1,27 +1,64 @@
-import React, { useEffect, useState } from "react";
-import { TextDescription } from "../../libs";
+import React, { useEffect, useMemo, useState } from "react";
+import { TextColor, TextDescription } from "../../libs";
 import "./Hope.scss";
 import { Card } from "antd";
 import dayjs from "dayjs";
 
 export default function Hope() {
-  const [date, setDate] = useState(dayjs("01/12/2023", "DD/MM/YYYY"));
+  const [date, setDate] = useState(
+    dayjs("12/15/2023", "DD/MM/YYYY").diff(dayjs(), "s"),
+  );
+  const totalDays = useMemo(() => Math.floor(date / (60 * 60 * 24)), [date]);
+  const totalHours = useMemo(
+    () => Math.floor((date - totalDays * 60 * 60 * 24) / (60 * 60)),
+    [date, totalDays],
+  );
+  const totalMinus = useMemo(
+    () =>
+      Math.floor((date - totalDays * 60 * 60 * 24 - totalHours * 60 * 60) / 60),
+    [date, totalDays, totalHours],
+  );
+  const totalSeconds = useMemo(
+    () =>
+      Math.floor(
+        date -
+          totalDays * 60 * 60 * 24 -
+          totalHours * 60 * 60 -
+          totalMinus * 60,
+      ),
+    [date, totalDays, totalHours, totalMinus],
+  );
+
   useEffect(() => {
-    setInterval(() => {
-      setDate(dayjs());
+    setTimeout(() => {
+      setDate((prevState) => prevState - 1);
     }, 1000);
-  }, []);
+  }, [date]);
+
   return (
     <div className="Hope">
       <TextDescription className="text-center">
         Mình cùng đếm ngược thời gian
-        <div className="countdown">
-          <Card>{date.get("d")}</Card>
-          <Card>{date.get("h")}</Card>
-          <Card>{date.get("m")}</Card>
-          <Card>{date.get("s")}</Card>
-        </div>
       </TextDescription>
+
+      <div className="countdown">
+        <Card className="card-time" size="small">
+          <TextColor>{totalDays}</TextColor>
+          <TextDescription>NGÀY</TextDescription>
+        </Card>
+        <Card className="card-time" size="small">
+          <TextColor>{totalHours}</TextColor>
+          <TextDescription>GIỜ</TextDescription>
+        </Card>
+        <Card className="card-time" size="small">
+          <TextColor>{totalMinus}</TextColor>
+          <TextDescription>PHÚT</TextDescription>
+        </Card>
+        <Card className="card-time" size="small">
+          <TextColor>{totalSeconds}</TextColor>
+          <TextDescription>GIÂY</TextDescription>
+        </Card>
+      </div>
 
       <TextDescription className="note text-center">
         Đám cưới của chúng tôi chắc chắn sẽ kém vui đi rất nhiều nếu thiếu đi sự
