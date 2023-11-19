c93a2d0fa59767e665117df66c396b41f7c86bcf
vinhdt912 <dothanhvinh0912@gmail.com>
2023-11-19 11:22:44 +0700

    change address

--- src/components/Map/GoogleMap.tsx
+++ src/components/Map/GoogleMap.tsx
@@ -20,7 +20,7 @@ export default function GoogleMap() {
         <TextColor className="text-center">Địa chỉ nhà trai</TextColor>
 
         <Iframe
-          url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d232.58288211419224!2d105.84561931789814!3d21.139416355288137!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313500e43bf5a929%3A0x62d55cd6dc3a005a!2zQ-G7rWEgSMOgbmcgU8ahbiBTxqFuIFTDuW5n!5e0!3m2!1svi!2s!4v1700278614704!5m2!1svi!2s"
+          url="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3721.329714418955!2d105.84313107525958!3d21.13927298053785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjHCsDA4JzIxLjQiTiAxMDXCsDUwJzQ0LjUiRQ!5e0!3m2!1svi!2s!4v1700367666641!5m2!1svi!2s"
           width="100%"
           height="450"
           frameBorder={0}
